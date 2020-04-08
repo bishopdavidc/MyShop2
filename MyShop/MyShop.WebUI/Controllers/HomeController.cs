9f9e116ee06f95328ad03a7d98d8340853f5544d
bishopdavidc <fader84@gmail.com>
2020-04-08 12:24:18 -0400

    Slight update

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -19,7 +19,12 @@ namespace MyShop.WebUI.Controllers
 
             return View();
         }
+        public ActionResult Test()
+        {
+            ViewBag.Message = "Your application description page.";
 
+            return View();
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
